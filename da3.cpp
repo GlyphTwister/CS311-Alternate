8930629be5ad3871282dbf8fb47461028bcd3a9a
GlyphTwister <acco7887@gmail.com>
2024-09-22 12:58:20 -0800

    Created da3.cpp

---
+++ da3.cpp
@@ -0,0 +1,24 @@
+// da3.cpp
+// Kohlby Vierthaler
+// 2024/09/22
+// Source file for Potpourri
+
+#include "da3.hpp"     // For Assignment 3 prototypes & templates
+
+#include <functional>
+using std::function;
+
+
+void didItThrow(const function<void()> & ff,
+                bool & threw)
+{
+    // TODO: WRITE THIS!!!
+}
+
+
+int gcd(int a,
+        int b)
+{
+    return 42;  // DUMMY
+    // TODO: WRITE THIS!!!
+}
