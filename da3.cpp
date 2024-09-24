4a25e4db1376053a374eeba493d978fef6c5d93a
GlyphTwister <acco7887@gmail.com>
2024-09-24 05:48:37 +0000

    Completed Assignment

--- da3.cpp
+++ da3.cpp
@@ -1,16 +1,14 @@
 // da3.cpp
 // Kohlby Vierthaler
-// 2024/09/22
+// 2024/09/24
 // Source file for Potpourri
 
 #include "da3.hpp"     // For Assignment 3 prototypes & templates
-
 #include <functional>
 using std::function;
-#include <exception>
-using std::exception;
 
 
+// Exception throwing function
 void didItThrow(const function<void()> &ff, bool &threw) {
     
     try {
@@ -19,12 +17,10 @@ void didItThrow(const function<void()> &ff, bool &threw) {
         return;
     }
 
-    catch (exception & e) {
+    catch (...) {
         threw = true;
         throw;
     }
-
-    //return;
 }
 
 // Recursive function
--- da3.hpp
+++ da3.hpp
@@ -1,6 +1,6 @@
 // da3.hpp
 // Kohlby Vierthaler
-// 2024/09/22
+// 2024/09/24
 // Header file for Potpourri
 
 #ifndef FILE_DA3_HPP_INCLUDED
@@ -9,13 +9,15 @@
 #include "llnode.hpp"  // For LLNode
 #include <cstddef>     // For std::size_t
 #include <functional>  // For std::function
-#include <stdexcept>
+#include <stdexcept>   // For std::out_of_range
+#include <iostream>    // For std::cerr and std::endl
 
 
 // Looks up a linked list
 template <typename ValueType>
 ValueType lookup(const LLNode<ValueType> *head, std::size_t index) {
     
+    // Find size of linked list
     auto h = head;
     std::size_t size = 0;
     while (h != nullptr) {
@@ -27,13 +29,18 @@ ValueType lookup(const LLNode<ValueType> *head, std::size_t index) {
         throw std::out_of_range("Index out of range");
     }
 
-    try {
-        return index;
-    } catch (const std::out_of_range &e) {
-        return e.what();
+    // Find node to index
+    const LLNode<ValueType> *node = head; 
+    for (std::size_t i = 0; i < index; i++) {
+        node = node->_next;
     }
 
-    //return index;  // DUMMY
+    try {
+        return node->_data;
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Index is out of range " << e.what() << std::endl;
+        throw;
+    }
 }
 
 
@@ -41,36 +48,31 @@ ValueType lookup(const LLNode<ValueType> *head, std::size_t index) {
 void didItThrow(const std::function<void()> &ff, bool &threw);
 
 
+// Check if elements are sorted
 template <typename FDIter>
 bool checkSorted(FDIter first, FDIter last) {
     
-    FDIter previous = first;
-
-    if (first < last) {
-
-        ++first;
-
-        while (first != last) {
-            //++first;
-
-            if (*first < *previous) {
-                return false;
-            }
-            previous = first;
-            ++first;
-        }
+    // Empty range case
+    if (first == last) {
         return true;
-        
-    } else {
-        return false;
     }
+
+    FDIter previous = first;
+    ++first;
+
+    // Check if elements are out of order
+    while (first != last) {
+        if (*first < *previous) {
+            return false;
+        }
+        previous = first;
+        ++first;
+    }
+
+    // If all elements are sorted correctly, return true
     return true;
 }
 
-template <typename FDIter>
-bool operator<(const FDIter &first, const FDIter &other) {
-    return first < other;
-}
 
 // Implementation in source file
 int gcd(int a, int b);
